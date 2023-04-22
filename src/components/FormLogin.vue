2bf349c901375c9ce91c17ac778faf7cf6092622
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2023-04-21 22:06:22 -0300

    se agrega validacion de datos en formulario login

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -4,16 +4,16 @@
             <v-container class="">
                 <v-row>
                     <v-col cols="12" md="4">
-                        <v-text-field label="E-mail" required></v-text-field>
+                        <v-text-field v-model="email" label="E-mail" required></v-text-field>
                     </v-col>
                     <v-col cols="12" md="4">
-                        <v-text-field label="Contraseña" type="password" required></v-text-field>
+                        <v-text-field v-model="password" label="Contraseña" type="password" required></v-text-field>
                     </v-col>
                     <v-col cols="6" class="text-center">
             <v-btn :to="{name:'home'}" class="mx-2">CANCELAR</v-btn>
           </v-col>
           <v-col cols="6" class="text-center">
-            <v-btn :to="{name:'profile'}" type="submit" class="mx-2">INGRESAR</v-btn>
+            <v-btn @click.prevent="authUser" type="submit" class="mx-2">INGRESAR</v-btn>
           </v-col>
                 </v-row>
             </v-container>
@@ -21,4 +21,22 @@
     </v-sheet>
 </template>
 
-<script></script>
+<script setup>
+    import {ref} from 'vue'
+    import { getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+
+    let email = ref("");
+    let password = ref("")
+
+
+    const authUser = () => {
+        const auth = getAuth()
+        signInWithEmailAndPassword(auth, email.value, password.value).then(()=> {
+            alert('Éxito!')
+        })
+        .catch((error) => {
+            alert("Error: " + error.message)
+        })
+    }
+
+</script>
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -9,6 +9,20 @@ import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
 import pinia from '../store'
 import router from '../router'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBczlNDMaxROc_8S0s6n3Gc4WL6h4MMKlA",
+    authDomain: "curso-vue-f2ffd.firebaseapp.com",
+    projectId: "curso-vue-f2ffd",
+    storageBucket: "curso-vue-f2ffd.appspot.com",
+    messagingSenderId: "978143218416",
+    appId: "1:978143218416:web:9a88cfb059a3ef7c421035",
+    measurementId: "G-D1VWF8BJN1"
+  };
+
+initializeApp(firebaseConfig);
+
 
 export function registerPlugins (app) {
   loadFonts()
