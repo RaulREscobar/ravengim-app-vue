d600bb729d75a45a8237269dfd9184fc77576728
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2023-08-21 14:38:25 -0300

    se arreglan enlases de imagenes del carrusel

--- src/components/CarruselHome.vue
+++ src/components/CarruselHome.vue
@@ -4,7 +4,7 @@
         <v-carousel-item 
         v-for="(img, i) in imgCarruselHome" 
         :key="i" 
-        :src="img.src"
+        :src=getImageUrl(img.name)
         cover
         >
         </v-carousel-item>
@@ -17,18 +17,14 @@
 
 <script setup>
 
+const getImageUrl = (name) => {
+    return new URL(`../assets/${name}.png`, import.meta.url).href
+}
+
 const imgCarruselHome = [
-    {
-        src: './src/assets/fotoGim1.png',
-    },
-    {
-        src: './src/assets/fotoGim2.png',
-    },
-    {
-        src: './src/assets/fotoGim3.png',
-    },
-    {
-        src: './src/assets/fotoGim4.png',
-    },
+    { name: "fotoGim1" },
+    { name: "fotoGim2" }, 
+    { name: "fotoGim3" }, 
+    { name: "fotoGim4" }, 
 ]
 </script>
