1b7bf5463f367e5d077c39cd763ec847894ace58
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2023-08-21 22:07:53 -0300

    se modifican datos de contacto

--- src/components/Ubication.vue
+++ src/components/Ubication.vue
@@ -3,7 +3,7 @@
     <p class="text-center">
         Nuestro Gimnasio esta ubicado en :
         <br />
-        Moreno 1361, Villa Rosa
+        Moreno 1365, Villa Rosa
     </p>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3291.342672083118!2d-58.87407162446193!3d-34.41804994771625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc9da1cec66073%3A0x4d815dd6ba304f34!2sMoreno%201361%2C%20B1631DJC%20Villa%20Rosa%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1692637624967!5m2!1ses-419!2sar" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     <p class="text-center">
@@ -11,9 +11,7 @@
         <br />
         de Lunes a Viernes
         <br />
-        De 8hs a 14hs y de 15hs a 21 hs
-        <br />
-        Los Sabados de 08hs a 13hs
+        De 8hs a 13hs y de 14hs a 21 hs
     </p>
 </template>
 
