22cd4d59ede0dcc383ec542dd3cbc230c1b41347
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2023-10-11 18:13:04 -0300

    se agrega información al componente status

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -2,10 +2,11 @@
     <v-container class="text-center">
         <v-icon icon="mdi-check-circle" color="success" size="200" v-if="!latePayment"></v-icon>
         <v-icon icon="mdi-close-circle" color="red" size="200" v-else></v-icon>
-        <h3>{{ name }}</h3>
-        <h3>{{ lastName }}</h3>
-        <h3>{{ phone }}</h3>
-        <h3>{{ !latePayment }}</h3>
+        <h3 class="text-uppercase">{{ name }} {{ lastName }}</h3>
+        <h3 v-if="latePayment">Falta de pago</h3>
+        <h3>Su ultimo pago fue el día: </h3>
+        <h2> {{ datePay.toLocaleString('es-ES') }} </h2>
+        <h2>Por un monto de ${{ valuePay }}</h2>
         <v-btn @click="goToHome" :color="latePayment ? 'red' : 'success'">Volver</v-btn>
     </v-container>
 </template>
@@ -24,6 +25,8 @@ const name = ref("");
 const lastName = ref("");
 const phone = ref("");
 const latePayment = ref(false);
+const datePay = ref("NO HAY REGISTRO DE PAGO ANTERIORES");
+const valuePay = ref(0);
 
 //Logica para saber si esta al dia con los pagos
 const now = new Date;
@@ -41,23 +44,24 @@ const userRef = await getDocs(userQuery)
 
 try {
     userRef.forEach(user => {
-    //Creo una variable para guardar la fecha del ultimo pago.
-    const datePay = user.data().payments[0].date.toDate()
-    //Si la fecha de pago es de este mes seteamos latePayment en false
-    if (datePay < firstDay && now < thenthDay) {
-        latePayment.value = false
-    } else if (datePay > firstDay && now > thenthDay) {
-        latePayment.value = true
-    } else if (datePay > firstDay && now < thenthDay) {
-        latePayment.value = false;
-    } else {
-        latePayment.value = true;
-    }
+        //Creo una variable para guardar la fecha y el valor del ultimo pago.
+        datePay.value = user.data().payments[0].date.toDate()
+        valuePay.value = user.data().payments[0].value
+        //Si la fecha de pago es de este mes seteamos latePayment en false
+        if (datePay.value < firstDay && now < thenthDay) {
+            latePayment.value = false
+        } else if (datePay.value > firstDay && now > thenthDay) {
+            latePayment.value = true
+        } else if (datePay.value > firstDay && now < thenthDay) {
+            latePayment.value = false;
+        } else {
+            latePayment.value = true;
+        }
 
-    name.value = user.data().name;
-    lastName.value = user.data().lastName;
-    phone.value = user.data().phone;
-})
+        name.value = user.data().name;
+        lastName.value = user.data().lastName;
+        phone.value = user.data().phone;
+    })
 } catch (error) {
     console.error(error)
     latePayment.value = true;
