50e8ce9eb14fcee065e83933874740d44a7ff56b
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2024-01-13 21:02:44 -0300

    se crea formulario de edicion de contraseña

---
+++ src/components/FormChangePass.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-sheet width="300" class="mx-auto mt-3">
+        <v-form fast-fail @submit.prevent="changePassword">
+            <v-container class="">
+                <v-row>
+                    <v-col cols="12" md="4">
+                        <v-text-field v-model="newPass" label="Nueva contraseña" type="text" required></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="4">
+                        <v-text-field v-model="confirmPass" label="Confirmar contraseña" type="text"
+                            required></v-text-field>
+                    </v-col>
+                    <v-col cols="6" class="text-center">
+                        <v-btn @click.prevent="goToBack" class="mx-2">Cancelar</v-btn>
+                    </v-col>
+                    <v-col cols="6" class="text-center">
+                        <v-btn @click.prevent="changePassword(newPass)" :loading="loading" type="submit"
+                            class="mx-2">Cambiar</v-btn>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-form>
+    </v-sheet>
+</template>
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goToBack = () => router.go(-1);
+
+const loading = ref(false);
+const newPass = ref('');
+const confirmPass = ref('');
+
+
+const changePassword = (newPass) => {
+    loading.value = true;
+
+}
+</script>
--- src/components/FormData.vue
+++ src/components/FormData.vue
@@ -3,7 +3,7 @@
         <v-card>
             <v-card-title>
                 Mis datos
-                <BtnEditData :url= url />
+                <BtnEditData :url=url />
             </v-card-title>
             <v-row>
                 <v-col cols="6">
@@ -16,7 +16,8 @@
                     <v-card-item title="Apellido" :subtitle=lastName></v-card-item>
                     <v-card-item title="E-mail" :subtitle=email></v-card-item>
                     <v-card-item title="Socio Nro" :subtitle=nroSocio></v-card-item>
-                    <v-btn class="text-caption" density="compact" variant="plain" >Cambiar Contraseña</v-btn>
+                    <v-btn @click="goToUrl()" class="text-caption" density="compact" variant="plain">Cambiar
+                        Contraseña</v-btn>
                 </v-col>
             </v-row>
         </v-card>
@@ -28,6 +29,10 @@ import BtnEditData from './BtnEditData.vue';
 import { useAuthStore } from '@/store/authStore';
 import { db } from '@/firebase';
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+//Vue-Router
+const router = useRouter();
 
 //Stores
 const authStore = useAuthStore();
@@ -44,5 +49,6 @@ const email = ref(userRef.data().email);
 const nroSocio = ref(userRef.data().nroSocio);
 
 //url a ir para la edición
-const url = `users/${nroSocio.value.slice(0,-2)}`
+const url = `users/${nroSocio.value.slice(0, -2)}`
+const goToUrl = () => router.push(`users/change-pass/${nroSocio.value.slice(0, -2)}`);
 </script>
